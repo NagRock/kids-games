ab150b9ca52543973ad6446fdec31078762f51d3
Maciej Kuster <maciej.kuster@dynatrace.com>
2020-02-20 18:27:26 +0100

    some styling and showing random images to remember

--- src/app/memory-game/remember-random-images-screen/remember-random-images-screen.component.ts
+++ src/app/memory-game/remember-random-images-screen/remember-random-images-screen.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {interval} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-remember-random-images-screen',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./remember-random-images-screen.component.scss']
 })
 export class RememberRandomImagesScreenComponent implements OnInit {
+  randomImageSource: string;
 
   constructor() { }
 
   ngOnInit() {
+    const fiveSeconds = 5000;
+    const numberOfImages = 2 + 1;
+    const numberOfSubImages = 4 + 1;
+    interval(fiveSeconds).pipe(
+      tap(() => {
+        const randomImage = Math.floor(Math.random() * (numberOfImages - 1) + 1);
+        const randomSubImage = Math.floor(Math.random() * (numberOfSubImages - 1) + 1);
+        this.randomImageSource = `assets/images/${randomImage}-${randomSubImage}.jpg`;
+      }),
+    ).subscribe();
   }
 
 }
